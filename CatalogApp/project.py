0d28e7a4697b5f30cfc876ec9b11ef9bb966d677
Subhash <subgutti@gmail.com>
2016-02-29 17:30:53 -0800

    fix: FB and Google client secrets json was not read properly
    The client secrets json files didn't have the absolute path
    defined in code. And becaues of that it failed to read the
    file and throws file not found exception.
    
    As required provide the full absolute path.

--- CatalogApp/project.py
+++ CatalogApp/project.py
@@ -117,10 +117,10 @@ def fbconnect():
     access_token = request.data
     print "access token received %s " % access_token
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+    app_id = json.loads(open(os.path.join(os.path.dirname(__file__), "fb_client_secrets.json"), 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+        open(os.path.join(os.path.dirname(__file__), 'fb_client_secrets.json'), 'r').read())['web']['app_secret']
     url = "https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s" % (
         app_id, app_secret, access_token)
     h = httplib2.Http()
@@ -194,7 +194,7 @@ def gconnect():
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets(os.path.join(os.path.dirname(__file__), 'client_secrets.json'), scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
@@ -223,7 +223,7 @@ def gconnect():
         return response
 
     # Verify that the access token is valid for this app.
-    client_id = json.loads(open('client_secrets.json', 'r').read())[
+    client_id = json.loads(open(os.path.join(os.path.dirname(__file__), 'client_secrets.json'), 'r').read())[
         'web']['client_id']
     if result['issued_to'] != client_id:
         response = make_response(
