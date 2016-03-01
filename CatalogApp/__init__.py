5e797e0812528cac395540228c4122ea7439bbc7
Subhash <subgutti@gmail.com>
2016-03-01 06:37:35 -0800

    docs: Add documentation to __init__.py

--- CatalogApp/__init__.py
+++ CatalogApp/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Import the app's main configuration file
 import project
 
 if __name__ == "__main__":
+	# Listen on all public IPs
     app.run(host='0.0.0.0')
